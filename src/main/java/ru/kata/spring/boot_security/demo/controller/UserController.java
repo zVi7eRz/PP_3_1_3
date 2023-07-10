6b425581089eaf8360a941a52a97a62b0851081d
zVi7eRz <vi7er.7@gmail.com>
2023-07-10 16:40:39 +0300

    Controller

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repository.RoleRepository;
@@ -13,11 +12,9 @@ import ru.kata.spring.boot_security.demo.service.UserService;
 @Controller
 @RequestMapping("user")
 public class UserController {
-    private final UserService userService;
     private final RoleRepository roleRepository;
 
-    public UserController(UserService userService, RoleRepository roleRepository) {
-        this.userService = userService;
+    public UserController(RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
     }
 
